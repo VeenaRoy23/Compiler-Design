5ba9f1389f3dd3fcd6cd3d2abd8a686c7dc69924
VeenaRoy23 <127951652+VeenaRoy23@users.noreply.github.com>
2025-10-30 18:54:12 +0530

    Refactor Operator Precedence Parser implementation

--- OperatorPrecedenceParser.c
+++ OperatorPrecedenceParser.c
@@ -1,98 +1,110 @@
 #include<stdio.h>
 #include<string.h>
 
-int main()
-{
-	int n,p,q;
-	int top=-1,flag;
-	char ter[20],ch,x,y,symbol;
-	char opt[20][20];
-	char input[30],stack[30];
-	printf("Enter the no. of terminals:");
-	scanf("%d",&n);
-	printf("Enter the terminals:\n");
-	for(int i=0;i<n;i++)
-	{
-		scanf(" %c",&ter[i]);
-	}
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			printf("Enter precedence of %c to %c: ",ter[i],ter[j]);
-			scanf(" %c",&opt[i][j]);
-		}
-	}
-do{
-    flag=0;
-	printf("Enter the input string bounded by $: ");
-	scanf("%s",input);
-	
-	int j=0;
-	stack[++top]=input[j++];
-	
-	do{
-		for(int k=0;k<n;k++)
-		{
-			if(stack[top]==ter[k]){
-				p=k;
-				
-				break;
-			}
-		} 
-		
-		for(int k=0;k<n;k++)
-		{
-			if(input[j]==ter[k]){
-				q=k;
-				
-				break;
-			}
-		} 
-		ch=opt[p][q];
-	//	printf("%c %c %c\n",stack[top],ch,input[j]);
-		
-		if(ch=='-')
-		{
-			printf("Rejected\n");
-			flag=1;
-			break;
-		}
-		
-		else if(ch=='<'||ch=='=')
-		{
-			stack[++top]=input[j++];
-		}
-		
-		else {
-			do{
-				x=stack[top--];
-				y=stack[top];
-				
-				for(int k=0;k<n;k++)
-				{
-					if(x==ter[k]){
-						p=k;
-						break;
-					}
-				}	 
-		
-				for(int k=0;k<n;k++)
-				{
-					if(y==ter[k]){
-						q=k;
-						break;
-					}
-				} 
-				
-				symbol=opt[q][p];
-				
-			}while(symbol!='<' && symbol!='=');
-		}
-		
-	}while(stack[top]!='$' && input[j]!='$');
-	if(flag==0)
-	    printf("Accepted\n");
-	
-}while(strcmp(input,"exit")!=0);
-	
+int main(){
+    char ip[30],input[30],stack[30],prec[30][30],symbol;
+    int n;
+    printf("Enter no. of ip: ");
+    scanf("%d",&n);
+    printf("Enter:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf(" %c",&ip[i]);
+    }
+    printf("Precedence: \n");
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            printf("%c to %c: ",ip[i],ip[j]);
+            scanf(" %c",&prec[i][j]);
+        }
+    }
+    
+
+    
+    do{
+        printf("String : ");
+        scanf("%s",input);
+        int p,q,top=-1,j=0,flag=0;
+        char ch;
+        int s=1;
+        
+        for(int k=0;input[k]!='\0';k++){
+            char cur=input[k-1];
+            char next=input[k];
+            if((cur=='+' || cur=='*') && (next=='+' || next=='*')){
+                flag=1;
+                break;
+            }
+        }
+        
+        if(flag==1){
+            printf("Rejected\n");
+            continue;
+        }
+        
+        
+        
+        flag=0;
+        
+        stack[++top]=input[j++];
+        
+        do{
+            for(int k=0;k<n;k++)
+            {
+                if(stack[top]==ip[k]){
+                    p=k;
+                    break;
+                }
+            }
+            
+            for(int k=0;k<n;k++)
+            {
+                if(input[j]==ip[k]){
+                    q=k;
+                    break;
+                }
+            }
+            
+            ch=prec[p][q];
+            
+            if(ch=='-'){
+                flag=1;
+                break;
+            }
+            
+            else if(ch=='<' || ch=='='){
+                stack[++top]=input[j++];
+            }
+            
+            else{
+                do{
+                    for(int k=0;k<n;k++)
+                    {
+                        if(stack[top--]==ip[k]){
+                            p=k;
+                            break;
+                        }
+                    }
+                    
+                    for(int k=0;k<n;k++)
+                    {
+                        if(stack[top]==ip[k]){
+                            q=k;
+                            break;
+                        }
+                    }
+                    
+                    symbol=prec[q][p];
+                    
+                }while(symbol!='<' && symbol!='=');
+            }
+        }while(!(stack[top]=='$' && input[j]=='$'));
+        
+        if(flag==0)
+            printf("Accepted\n");
+        else
+            printf("Rejected\n");
+    }while(strcmp(input,"E")!=0);
 }
-		
